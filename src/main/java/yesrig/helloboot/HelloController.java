12ccf4face0b03cc05bace9afceae274bca76e33
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-31 23:17:44 +0900

    7/31 (ApplicationContext)

--- src/main/java/yesrig/helloboot/HelloController.java
+++ src/main/java/yesrig/helloboot/HelloController.java
@@ -1,5 +1,9 @@
 package yesrig.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,22 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
     private final HelloService helloService;
-
-    public HelloController(HelloService helloService) {
+    private ApplicationContext applicationContext;
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping
-    @ResponseBody
     public String hello(String name) {
-        if (name == null) { //Object.requireNonNull() 안돼서 if, else로 대체
-            return "Object is null";
-        } else {
-            return helloService.sayHello(name);
-        }
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
+
 }
+
