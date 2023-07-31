a673e9c78676b065de950cf39f1c8b649cf6cbd1
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-31 23:04:23 +0900

    7/31 (Component)

--- src/main/java/yesrig/helloboot/HellobootApplication.java
+++ src/main/java/yesrig/helloboot/HellobootApplication.java
@@ -25,16 +25,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Configuration
+@ComponentScan
 public class HellobootApplication {
-	@Bean
-	public HelloController helloController(HelloService helloService){
-		return new HelloController(helloService);
-
-	}
-	@Bean
-	public HelloService helloService(){
-		return new SimpleHelloService();
-	}
 
 	public static void main(String[] args) {
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
--- src/main/java/yesrig/helloboot/SimpleHelloService.java
+++ src/main/java/yesrig/helloboot/SimpleHelloService.java
@@ -1,5 +1,9 @@
 package yesrig.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name){
