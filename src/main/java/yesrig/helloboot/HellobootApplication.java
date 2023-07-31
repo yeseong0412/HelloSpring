3adac3d108e3f2859f41bcba8a875ce6b3c2b74f
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-31 23:39:41 +0900

    7/31 (Application)

--- src/main/java/yesrig/helloboot/HellobootApplication.java
+++ src/main/java/yesrig/helloboot/HellobootApplication.java
@@ -1,5 +1,6 @@
 package yesrig.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -19,7 +20,7 @@ public class HellobootApplication {
 		return new DispatcherServlet();
 	}
 	public static void main(String[] args) {
-		MySpringApplication.run(HellobootApplication.class, args);
+		SpringApplication.run(HellobootApplication.class, args);
 	}
 
 
