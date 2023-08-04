b89bbc813405116b35914b7c03baac067c7c2684
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-04 18:41:41 +0900

    8/4 (Config Annotation)

--- src/main/java/yesrig/helloboot/HellobootApplication.java
+++ src/main/java/yesrig/helloboot/HellobootApplication.java
@@ -8,17 +8,8 @@ import org.springframework.context.annotation.*;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class HellobootApplication {
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory(){
-		return new TomcatServletWebServerFactory();
-	}
-	@Bean
-	public DispatcherServlet dispatcherServlet(){
-		return new DispatcherServlet();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 	}
